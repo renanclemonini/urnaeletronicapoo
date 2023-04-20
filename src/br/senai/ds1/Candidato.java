9e209f5de79e2457ed74ed965575164a93d296c5
Aluno SENAI LAURO <aluno@SENAI-LAURO.LOCAL>
2023-04-20 09:34:41 -0300

    inicio

---
+++ src/br/senai/ds1/Candidato.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.senai.ds1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author aluno
+ */
+public class Candidato {
+    
+    private String nome;
+    private String vice;
+    private int numero;
+    private String partido;
+
+    public Candidato() {
+    }
+
+    public Candidato(String nome, String vice, int numero, String partido) {
+        this.nome = nome;
+        this.vice = vice;
+        this.numero = numero;
+        this.partido = partido;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getVice() {
+        return vice;
+    }
+
+    public void setVice(String vice) {
+        this.vice = vice;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPartido() {
+        return partido;
+    }
+
+    public void setPartido(String partido) {
+        this.partido = partido;
+    }
+    
+    
+    
+    
+    
+    
+}
